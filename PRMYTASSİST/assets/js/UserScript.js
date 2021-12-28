5903d3e2ce9a94f516276d46cae90c9e87810ee4
Beyza Nur Özkan <beyzanurozkann877@gmail.com>
2021-12-28 20:20:54 +0300

    Beyza

--- PRMYTASSİST/assets/js/UserScript.js
+++ PRMYTASSİST/assets/js/UserScript.js
@@ -196,10 +196,6 @@
                             result = `<a onclick="EditUser(` + full['ID'] + `)" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Düzenle">
                           <i class="la la-edit"></i>
                          </a>
-                        <a onclick="InDontShow(` + full['ID'] + `)" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Durumu Pasifleştir">
-                          <i class="la la-eye-slash"></i>
-                         </a>
-                        
                          <a onclick="DeleteUsers(` + full['ID'] + `)" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Sil">
                          <i class="la la-trash"></i>
                          </a>
@@ -345,10 +341,6 @@ function getRoles() {
                             result = `<a onclick="EditRoles(` + full['ID'] + `)" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Düzenle">
                          <i class="la la-edit"></i>
                          </a>
-                 
-                         <a onclick="InDontShowRoles(` + full['ID'] + `)" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Durumu Pasifleştir">
-                          <i class="la la-eye-slash"></i>
-                         </a>
                          <a onclick="DeleteRoles(` + full['ID'] + `)" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Sil">
                          <i class="la la-trash"></i>
                          </a>
@@ -426,5 +418,226 @@ function getRoles() {
         t.column(0, { page: 'current' }).nodes().each(function (cell, i) {
             cell.innerHTML = i + 1 + PageInfo.start;
         });
+    });
+};
+function EditUser(id) {
+    debugger
+    $.ajax({
+        type: "POST",
+        url: '/User/EditUser/' + id,
+        success: function (data) {
+            debugger
+            $('#userModal #CreatedUser').text(data['CreateUser']);
+            $('#userModal #CreatedDate').text(data['CreateDate']);
+            $('#userModal #UpdateUser').text(data['LastUpdateUser']);
+            $('#userModal #UpdateDate').text(data['LastUpdateDate']);
+            $('#userModal #EMail').val(data['EMail']);
+            $('#userModal #UserInformation').text(data['Name'] + ' ' + data['LastName']);
+
+            $('#userModal #userID').val(data['ID']);
+            $('#userModal #Visible').val(data['Visible']);
+            $('#userModal #UserName').val(data['Name']);
+            $('#userModal #UserLastName').val(data['LastName']);
+            $('#userModal #UserCountName').val(data['UserCountName']);
+            $('#userModal #UserPassword').val(data['Password']);
+            $('#userModal #userForBranchSelect #userForBranchSelectOptions').text(data['Branches']);
+            $('#userModal #userForBranchSelect #userForBranchSelectOptions').val(data['CurrentBranch']);
+
+            $('#userModal #userForRoleSelect #userForCategorySelectOptions').text(data['UserRole']);
+            $('#userModal #userForRoleSelect #userForCategorySelectOptions').val(data['RoleID']);
+            userForCategorySelectOptions
+            getBranches1(id);
+            $('#userModal').modal();
+        },
+        error: function (request, status, error) {
+            swal.fire("Hata!", "Bir sorun ile karşılaşıldı!", "error");
+        }
+    });
+};
+function getBranches1(id) {
+    debugger
+    var table = $('#kt_table_braches1');
+    var t = table.DataTable({
+        ajax: {
+            url: '/Branch/GetUserBranch1/' + id,
+            type: 'POST',
+            contentType: 'application/json; charset=utf-8',
+            dataType: "json",
+
+        },
+        "pageLength": 50,
+
+        columns: [
+            {
+                data: 'SN',
+                className: 'dt-center',
+                searchable: false,
+                orderable: false,
+            },
+            {
+                data: 'BranchName',
+                className: 'font-weight-bold',
+            },
+            { data: 'BranchCreatedDate' },
+            {
+                data: 'Durum',
+                className: 'dt-center',
+            },
+
+        ],
+        'stripeClasses': ['stripe1', 'stripe2'],
+        dom: `<'row'<'col-sm-6 text-left'f><'col-sm-6 text-right'B>>
+			<'row'<'col-sm-12'tr>>
+			<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
+
+        buttons: [{
+            extend: 'collection',
+            text: '<i class="flaticon2-gear" style= "font-size:16px; "></i>',
+            className: 'btn btn-outline-info dropleft-inline  pt-2 mt-0 pb-2 mb-0',
+            buttons: [
+                'excel',
+                'print',
+                'csv',
+                'pdf',
+                'copy',
+                'colvis',
+            ],
+        }],
+        columnDefs: [
+            {
+                targets: 0,
+
+
+                render: function (data, type, full, meta) {
+
+                    result = ``;
+
+                    return result;
+                }
+
+            },
+            {
+                targets: 2,
+                className: 'dt-center',
+                render: function (data, type, full, meta) {
+                    var id = full['BranchCreatedDate'];
+                    var result = moment(id).format('DD.MM.YYYY');
+                    return result;
+                },
+
+            },
+            {
+                targets: 1,
+                render: function (data, type, full, meta) {
+                    var returnHtml = `<div  style="width:100%"><b style="color:red; width:250px;"> ` + full['BranchName'] + `</b></div> `;
+                    return returnHtml;
+                },
+            },
+            {
+                targets: -1,
+                title: 'Durum',
+                //"language": {
+                //    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json"
+                //},
+                orderable: false,
+                "width": "5%",
+                render: function (data, type, full, meta) {
+
+                    return `
+                        <a onclick="CancelBranchUser(` + full['ID'] + `,` + id + `)" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Şubeden Çıkar">
+                          <i class="la la-close"></i>
+                        </a>`;
+                }
+            }
+        ],
+        responsive: true,
+
+        // Order settings
+        order: [[1, 'desc']],
+
+        responsive: false,
+        "scrollX": true,
+        stateSave: true,
+        orderCellsTop: true,
+        fixedHeader: true,
+        "destroy": true,
+
+    });
+    t.on('draw.dt', function () {
+        var PageInfo = $('#kt_table_braches1').DataTable().page.info();
+        t.column(0, { page: 'current' }).nodes().each(function (cell, i) {
+            cell.innerHTML = i + 1 + PageInfo.start;
+        });
+    });
+    table.on('change', '.kt-group-checkable', function () {
+        var set = $(this).closest('table').find('td:first-child .kt-checkable');
+        var checked = $(this).is(':checked');
+
+        $(set).each(function () {
+            if (checked) {
+                $(this).prop('checked', true);
+                $(this).closest('tr').addClass('active');
+            }
+            else {
+                $(this).prop('checked', false);
+                $(this).closest('tr').removeClass('active');
+            }
+        });
+    });
+
+    table.on('change', 'tbody tr .kt-checkbox', function () {
+        $(this).parents('tr').toggleClass('active');
+    });
+};
+function InDontShow(id) {
+    var table = $('#kt_table_branchFormat');
+    $.ajax({
+        type: "POST",
+        url: '/Branch/InDontShow/' + id,
+        success: function (data) {
+            swal.fire({
+                title: "Başarılı!",
+                text: data,
+                type: "success",
+                buttonsStyling: false,
+                confirmButtonText: "Tamam!",
+                confirmButtonClass: "btn btn-brand"
+            }).then(function (result) {
+                if (result.value) {
+                    table.DataTable().ajax.reload();
+                }
+            });
+        },
+        error: function (request, status, error) {
+            swal.fire("Hata!", "Bir sorun ile karşılaşıldı!", "error");
+        }
+    });
+};
+function DeleteUsers(id) {
+    var table = $('#kt_table_users');
+    swal.fire({
+        title: 'Silmek istediğinizden emin misiniz?',
+        text: "Bu İşlem Geri Alınamaz!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Evet, Sil!',
+        cancelButtonText: 'Hayır, İptal Et!',
+        reverseButtons: true
+    }).then(function (result) {
+        if (result.value) {
+            debugger
+            $.ajax({
+                type: "POST",
+                url: '/User/DeleteUser/' + id,
+                success: function (data) {
+                    table.DataTable().ajax.reload();
+                },
+            });
+            debugger
+        } else if (result.dismiss === 'cancel') {
+            swal.fire(
+                "İptal!", "Silme İşlemi İptal Edildi!", "error"
+            )
+        }
     });
 };
